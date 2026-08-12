8ed5244047a2562625e74b68d231079bba842cbd
aastikdas <aastikdas126@gmail.com>
2026-08-12 21:15:47 +0530

    cors policy updated

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
